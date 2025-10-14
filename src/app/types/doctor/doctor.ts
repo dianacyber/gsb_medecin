8c4875ec5ab2686a1cb82306dc50f304e8e647e5
gakou diana <dianagakou@gmail.com>
2025-10-14 12:55:55 +0200

    creation de l'interface medecin

--- src/app/types/doctor/doctor.ts
+++ src/app/types/doctor/doctor.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class Doctor {
   id: number;
-  firstName: string;
+  firstName: string | undefined;
   lastName: string;
   email: string;
   speciality: string;
---
+++ src/app/types/medecin/medecin.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Medecin } from './medecin';
+
+describe('Medecin', () => {
+  let component: Medecin;
+  let fixture: ComponentFixture<Medecin>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Medecin]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Medecin);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
