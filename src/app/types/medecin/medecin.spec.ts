d1ca7a5de38af196fb278c465daba5c3d801355f
diana.gakou <dianagakou@gmail.com>
2025-11-04 11:54:39 +0100

    feat(helpers): add convertMedecinToDoctor helper

--- src/app/types/medecin/medecin.spec.ts
+++ src/app/types/medecin/medecin.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Medecin } from './medecin';
+import { Medecin } from './medecin.interface';
 
 describe('Medecin', () => {
   let component: Medecin;
